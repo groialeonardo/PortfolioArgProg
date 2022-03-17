ac852b81a28e2dd5a3134ec5210f6f29bf2ecbd8
groialeonardo <groialeonardo@gmail.com>
2022-03-17 17:41:25 +0000

    se pusieron logos en header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header/header.component';
 import { LogoArgProgComponent } from './components/header/logo-arg-prog/logo-arg-prog.component';
+import { SociallinkComponent } from './components/header/sociallink/sociallink.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoArgProgComponent
+    LogoArgProgComponent,
+    SociallinkComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/header/sociallink/sociallink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sociallink',
+  templateUrl: './sociallink.component.html',
+  styleUrls: ['./sociallink.component.css']
+})
+export class SociallinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
