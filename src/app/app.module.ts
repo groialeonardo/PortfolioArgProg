85d1f9a214ef468f27362e06f15558b7e9a57719
groialeonardo <51366574+groialeonardo@users.noreply.github.com>
2022-03-17 15:25:10 -0300

    Se agregaron componentes header y logo arg-prgo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { LogoArgProgComponent } from './components/header/logo-arg-prog/logo-arg-prog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent
   ],
   imports: [
     BrowserModule
