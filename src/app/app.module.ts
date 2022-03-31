a051bef302a6cfd1e4048c2b878ecad4f14323b6
groialeonardo <groialeonardo@gmail.com>
2022-03-31 17:25:44 +0000

    se comienza a implementar el addexp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header/header.component';
@@ -30,6 +31,7 @@ import { AddexpComponent } from './components/experiencia/addexp/addexp.componen
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule
 
   ],
   providers: [],
--- src/app/components/experiencia/addexp/addexp.component.ts
+++ src/app/components/experiencia/addexp/addexp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter, Output  } from '@angular/core';
 
 @Component({
   selector: 'app-addexp',
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddexpComponent implements OnInit {
 
-  constructor() { }
+  @Output() addTaskEvent:EventEmitter<Task> = new EventEmitter()
+
+  nombreEmpresa:string ="";
+  fechaInicio:string = "";
+  fechaFin:string = "";
+  nombrePuesto:string="";
+  descripcion:string="";
+
+  showAddExp:boolean = true;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+
+    if(this.nombreEmpresa.length === 0){
+      alert("Please add a Task!");
+      return
+    }
+
+    const {nombreEmpresa,fechaInicio,fechaFin,nombrePuesto,descripcion} = this;
+
+    const newTask = {nombreEmpresa,fechaInicio,fechaFin,nombrePuesto,descripcion};
+
+    console.log(newTask)
+
+    //this.addTaskEvent.emit(newTask)
+
+
+  }
 }
